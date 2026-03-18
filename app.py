9467e3612b8a30fce256b4c425c86876e75e2ccb
ivanwonghs <ivanwong85@yahoo.com.hk>
2026-03-18 17:00:04 +0800

    Update title for sentiment analysis application

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def main():
     sentiment_pipeline = pipeline(model="ivanwonghs/trial_1")
     #replyMsg_pipeline = pipeline(model="microsoft/Phi-4-mini-instruct")
 
-    st.title("Sentiment Analysis with HuggingFace Spaces")
+    st.title("Muti-language Reply Message Generator For Negative Comment")
     st.write("Enter a sentence to analyze its sentiment:")
 
     user_input = st.text_input("")
